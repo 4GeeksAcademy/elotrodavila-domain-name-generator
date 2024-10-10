17902740cc1e8145b752a900125259e38644e237
Federico Davila <30817202+elotrodavila@users.noreply.github.com>
2024-10-10 00:00:19 +0000

    finished the domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our', 'a', 'your'];
+  let adj = ['great', 'big', 'little', 'super', 'last', 'super', 'mega'];
+  let noun = ['jogger', 'racoon', 'site', 'planet', 'asteroid', 'cat', 'dog', 'tornado', 'surprise'];
+
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
 };
